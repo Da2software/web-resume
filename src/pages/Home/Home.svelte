c0755537a9e02b2e54b00510661380f955f6c6dd
puh <puh@birkman.com>
2024-05-22 14:41:43 -0600

    Updates on description and contact information

--- src/pages/Home/Home.svelte
+++ src/pages/Home/Home.svelte
@@ -9,19 +9,25 @@
       <h1 id="name" class="my-2">Pedro Uh</h1>
     </div>
     <div class="info-desc">
-      <h6 id="info">
+      <h6 id="contact-information">
         <!-- 97840, Yucatan México | 9992056369 | pedro.gil.uh@gmail.com -->
-        Yucatan México | 9992056369 | pedro.gil.uh@gmail.com 
+        Yucatan, México | 9992056369 |
+        <a href="mailto:">pedro.gil.uh@gmail.com</a>
+        |
+        <a href="http://github.com/Da2software" target="_blank" rel="Github"
+          >Github</a
+        >
       </h6>
     </div>
     <Section title={"Professional Summary"}>
       <p class="mx-2 my-4">
         I bring over five years of extensive experience in software development,
-        specializing in different back-end technologies and building
-        applications architectures from scratch. My proficiency spans both SQL
-        and No-SQL databases, I worked directly with US-based clients, and I
-        collaborated with cross-functional teams in an Agile environment. I’m a
-        self-learner and l really enjoy sharing knowledge with others.
+        specializing in different back-end technologies such as Python(Django,
+        Flask and FastApi) or Nodejs(Express) and building applications
+        architectures from scratch. My proficiency spans both SQL and No-SQL
+        databases, I worked directly with US-based clients, and I collaborated
+        with cross-functional teams in an Agile environment. I’m a self-learner
+        and l enjoy sharing knowledge with others.
       </p>
     </Section>
 
@@ -53,6 +59,71 @@
         EndDate={"Current"}
       >
         <p>
+          <i class="sub-role">Intermediate to Advanced Full Stack Developer:</i>
+          <li>
+            Worked on software architectures using microservices and built
+            framework libraries for Angular. Also, developed backend solutions
+            in Python with Elasticsearch and SQL databases.
+          </li>
+          <li>
+            Provided comprehensive technical training to new and existing
+            DevOps, Developers, QA, and QA automation team members.
+          </li>
+          <li>
+            Improved a key feature for generating backend (Python) and frontend
+            (Angular) reports, significantly aiding in meeting delivery
+            timelines.
+          </li>
+          <li>
+            Developed an advanced custom UI editor in Angular and built a
+            complex dynamic data handler on the backend in Python, enabling
+            dynamic content creation for clients.
+          </li>
+          <li>
+            Created a pipeline system using Python and Jenkins for asynchronous
+            data transformation, optimizing response times and enhancing data
+            visualization.
+          </li>
+          <li>
+            Designed analysis to implement unit testing frameworks, and created
+            testing showcases and demos for client comprehension.
+          </li>
+          <li>
+            Developed an Elasticsearch node architecture analysis report to
+            provide actionable insights for DevOps optimization and risk
+            mitigation. Assisted the DevOps team in the CI/CD process and
+            troubleshooting server environment issues.
+          </li>
+          <li>
+            Migrated a project stack from Python, Flask, Elasticsearch, and
+            Angular to Python, Django, SQL Server, and Angular. Built a
+            microservice API from scratch using Python and Django to provide
+            necessary information to the frontend application.
+          </li>
+          <li>
+            Contributed to feature development, screen design, and custom
+            component creation using Vue.js.
+          </li>
+          <i class="sub-role">Junior Full Stack Developer:</i>
+          <li>
+            Developed the entire application architecture for a third US-based
+            client, creating a mental health-related web application using the
+            MEAN stack (MongoDB, Express, Node.js, Angular), PostgreSQL, and
+            Python. Also, created the main algorithm for the application.
+          </li>
+          <li>
+            Designed and implemented the complete PostgreSQL database
+            architecture.
+          </li>
+          <li>
+            Built main features for two projects simultaneously on the backend
+            (Node.js) and frontend (Angular), and successfully deployed both
+            projects to AWS environments.
+          </li>
+          <li>
+            Worked on ETL processes with asynchronous Python scripts, resulting
+            in increased efficiency and reduced processing times.
+          </li>
           <i class="sub-role">Internship Developer:</i>
           <li>
             Designed and implemented an SQL optimization and automation script,
@@ -61,69 +132,6 @@
           <li>
             I worked on UI changes for a project about prediction and assistance
             software for lawyers.
-          </li>
-          <i class="sub-role">Junior Full Stack Developer:</i>
-          <li>
-            I developed the entire application architecture project for a third
-            US-based client, for a mental health related web application using
-            MongoDB, Express, Node.js, Angular (MEAN), PostgreSQL, and Python,
-            and also I created an the main algorithm for the application.
-          </li>
-          <li>
-            Designed and implemented an entire PostgreSQL database architecture.
-          </li>
-          <li>
-            Built main features for two projects at the same time on the back
-            end (Node.js) and front-end (Angular) side, and also successfully
-            deployed both projects to AWS environments.
-          </li>
-          <li>
-            Developed advanced Python scripts for porting data from a PubMed API
-            to our application.
-          </li>
-          <i class="sub-role">Intermediate to Advanced Full Stack Developer:</i>
-          <li>
-            I worked on software architectures over microservices and building
-            framework libraries for Angular, also working on Python as a backend
-            with Elasticsearch and SQL as a database.
-          </li>
-          <li>
-            Provided comprehensive technical training to new and existing
-            DevOps, Developers, QA, and QA automation team members.
-          </li>
-          <li>
-            Improved a key feature for generating back-end (Python) and
-            front-end (Angular) reports, aiding in achieving delivery times.
-          </li>
-          <li>
-            Developed an advanced custom UI editor (Angular) and built a complex
-            dynamic data handler on the back-end (Python) to be rendered on the
-            front end, facilitating dynamic content creation for clients.
-          </li>
-          <li>
-            Developed a pipeline system using Python and Jenkins for
-            transforming data asynchronously, optimizing response times, and
-            data visualization.
-          </li>
-          <li>
-            Designed analysis to implement robust unit testing frameworks, and
-            created testing showcases and demos for client comprehension.
-          </li>
-          <li>
-            Developed an Elasticsearch node architecture analysis report to
-            provide actionable insights for DevOps optimization and risk
-            mitigation Facilitated a DevOps team in the CI/CD process and
-            troubleshooting environment server issues.
-          </li>
-          <li>
-            Ported a project stack from Python, Flask, Elasticsearch, and
-            Angular to Python, Django, SQL server, and Angular. Built a
-            microservice API from scratch using Python and Django to provide
-            necessary information to the front-end application.
-          </li>
-          <li>
-            Contributed to feature development, screen design, and custom
-            component creation using Vue.js.
           </li>
         </p>
       </Work>
@@ -230,7 +238,7 @@
     width: 100%;
   }
   .info-desc h6 {
-    font-size: .9rem;
+    font-size: 0.9rem;
     color: #616161;
     width: fit-content;
     margin: 0.5rem auto;
